23c6c905c229f6a4ec76ae00c6fb9f1fd455fa07
Miguel Marcano <miguel131299@gmail.com>
2025-05-03 20:13:53 +0200

    implement upload thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +31,56 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+
+	// Read all image data into memory
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not read file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// No chirp found
+			respondWithError(w, http.StatusNotFound, "No video with videoID", err)
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, "Could not read file", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized to upload thumbnail", nil)
+		return
+	}
+
+	// save thumbnail to global map
+	tn := thumbnail{
+		data:      imageData,
+		mediaType: contentType,
+	}
+	videoThumbnails[videoID] = tn
+
+	// update video metadata
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &url
+	cfg.db.UpdateVideo(video)
+	respondWithJSON(w, http.StatusOK, video)
 }
